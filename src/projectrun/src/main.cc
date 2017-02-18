11172b4420a148cb125c2757831b65dde478b5b5
Dotan Nahum <jondotan@gmail.com>
2017-02-18 14:35:39 +0200

    use clang complete for better atom lints

--- src/projectrun/src/main.cc
+++ src/projectrun/src/main.cc
@@ -1,7 +1,31 @@
 #include <iostream>
+
 #include "projectlib/project.h"
+
+using namespace std;
+
+int f(int a){
+    cout << "f" << a << endl;
+    return a;
+}
+
+int g(int a){
+    cout << "g" << a << endl;
+    return a;
+}
+
+function<int(int)> compose(function<int(int)> af, function<int(int)> ag){
+    auto fg = [af,ag](auto a){
+        return f(g(a));
+    };
+    return fg;
+}
+
+
 
 int main(void) {
   std::cout << project::api() << std::endl;
+  auto newfunc = compose(f,g);
+  newfunc(3);
   return 0;
 }
